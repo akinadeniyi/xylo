7ce0f52ea50031329f11547c2426d4a4a60b32c8
heykay7 <heykay7@gmail.com>
2019-10-18 10:41:30 +0100

    developing

--- js/profile.js
+++ js/profile.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
                 <td>${result[i].leaveType}</td>
                 <td>${result[i].startDate}</td>
                 <td>${result[i].endDate}</td>
-                <td><button id="update">Update</button></td>
+                <td><button type="button" class="btn btn-success btn-block btn-update" id="update">Update</button></td>
                 <td><button type="button" data-id=${result[i].id} class="btn btn-danger btn-block btn-delete">Delete</button></td>
             
                 </tr>`);
